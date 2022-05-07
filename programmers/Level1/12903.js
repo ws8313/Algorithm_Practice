773996915f159cc8376bf83cb906c196894c9a4e
ws8313 <ws8313@naver.com>
2022-05-08 02:17:06 +0900

    2022.05.08

--- programmers/Level1/12903.js
+++ programmers/Level1/12903.js
@@ -3,6 +3,7 @@ function solution(s) {
     if (s.length % 2 === 0) {
         return s[(s.length / 2) - 1] + s[(s.length / 2)];
     } else {
+        // return s[Math.floor(s.length / 2)];
         return s[parseInt(s.length / 2)];
     }
 }
