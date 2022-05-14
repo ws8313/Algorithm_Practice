f4d36682fa0b9035e85d712c6366e9d17a545065
ws8313 <ws8313@naver.com>
2022-05-14 22:46:53 +0900

    2022.05.14

--- programmers/Level1/68935.js
+++ programmers/Level1/68935.js
@@ -1,4 +1,4 @@
 // 프로그래머스 68935번 3진법 뒤집기
 function solution(n) {
-    return parseInt(n.toString(3).split("").reverse().join(''), 3);
+    return parseInt(n.toString(3).split('').reverse().join(''), 3);
 }
