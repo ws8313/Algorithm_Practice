45b4ec52dce13a42d158154c04a6dbeab46054e8
ws8313 <ws8313@naver.com>
2022-05-16 17:25:10 +0900

    feat: solve programmers 77884

---
+++ programmers/Level1/77884.js
@@ -0,0 +1,19 @@
+// 프로그래머스 77884번 약수의 개수와 덧셈
+function solution(left, right) {
+    var answer = 0;
+    
+    for (let i = left; i <= right; i++) {
+        var count = 0;
+        for (let j = 1; j <= i; j++) {
+            if (i % j == 0) {
+                count += 1;
+            }
+        }
+        if (count % 2 == 0) {
+            answer += i;
+        } else {
+            answer -= i;
+        }
+    }
+    return answer;
+}
