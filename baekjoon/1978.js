d9293ce7814e1b3f746ecba233462d0112cc16cb
ws8313 <ws8313@naver.com>
2022-05-19 21:00:35 +0900

    feat: solve boj 1978

---
+++ baekjoon/1978.js
@@ -0,0 +1,27 @@
+// 백준 1978번 소수 찾기
+const filePath = process.platform === 'linux' ? 'dev/stdin' : './예제.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+
+const N = input[0];
+const nums = input[1].split(' ');
+let arr = [];
+let answer = 0;
+
+function primeNumber(n) {
+    if (n < 2) {
+        return;
+    }
+
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+            return;
+        }
+    }
+    answer += 1;
+}
+
+for (let i = 0; i < N; i++) {
+    primeNumber(nums[i]);
+}
+
+console.log(answer);
