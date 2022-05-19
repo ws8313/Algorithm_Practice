383abea7886cf3b94582a0ecefbe2953bb2ed53a
ws8313 <ws8313@naver.com>
2022-05-19 23:58:14 +0900

    feat: boj 2108

--- baekjoon/2108.js
+++ baekjoon/2108.js
@@ -1,6 +1,6 @@
 // 백준 2108번 통계학
 const filePath = process.platform === 'linux' ? 'dev/stdin' : './예제.txt';
-const input = require('fs').readFileSync(filePath).toString().trim().split('\n')
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 const N = input.shift();
 const nums = input.map(Number);
 
