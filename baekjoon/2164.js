5633da105e4a8c80aad65edfc743f550e6260d9d
ws8313 <ws8313@naver.com>
2022-05-20 00:03:01 +0900

    feat: solve boj 2164

--- baekjoon/2164.js
+++ baekjoon/2164.js
@@ -3,15 +3,28 @@ const filePath = process.platform === 'linux' ? 'dev/stdin' : './예제.txt';
 const input = Number(require('fs').readFileSync(filePath).toString().trim());
 
 // 시간초과
-const arr = [];
-for (let i = 0; i < input; i++) {
-    arr[i] = i + 1;
-}
+// const arr = [];
+// for (let i = 0; i < input; i++) {
+//     arr[i] = i + 1;
+// }
 
-while(arr.length != 1) {
+// while(arr.length != 1) {
+//     arr.shift();
+//     arr.push(arr.shift());
+// }
+
+// console.log(arr[0]);
+
+// 규칙 (2진법 사용)
+// 1. n이 2^x 일 경우 값은 n
+// 2. (n - (n보다 작은 2^x)) * 2
+function solution(n) {
+    const arr = n.toString(2).split("");
     arr.shift();
-    arr.push(arr.shift());
+
+    const answer = parseInt(arr.join(""), 2);
+
+    return answer ? answer * 2 : n;
 }
 
-console.log(arr[0]);
-
+console.log(solution(input));
