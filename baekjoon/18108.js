d7bdca56c2de5944e870fb4d56969750f12b2434
ws8313 <ws8313@naver.com>
2022-06-09 02:45:01 +0900

    feat: solve boj 18108

--- baekjoon/18108.js
+++ baekjoon/18108.js
@@ -1,5 +1,5 @@
-// 백준 10926번 ??!
+// 백준 18108번 ??!
 const filePath = process.platform === 'linux' ? 'dev/stdin' : './예제.txt';
 const input = require('fs').readFileSync(filePath).toString().trim();
 
-console.log(input + '??!');
+console.log(input - 543);
