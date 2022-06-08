6b3ceba70e0b7a4bbc00476cfa4de1316eea9505
ws8313 <ws8313@naver.com>
2022-06-09 02:51:40 +0900

    feat: solve boj 25083

--- baekjoon/18108.js
+++ baekjoon/18108.js
@@ -1,4 +1,4 @@
-// 백준 18108번 ??!
+// 백준 18108번 1998년생인 내가 태국에서는 2541년생?!
 const filePath = process.platform === 'linux' ? 'dev/stdin' : './예제.txt';
 const input = require('fs').readFileSync(filePath).toString().trim();
 
