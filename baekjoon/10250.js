213f525359c9c59dc8309927a91d6095780807c2
ws8313 <ws8313@naver.com>
2022-06-14 17:48:17 +0900

    feat: solve boj 10250

---
+++ baekjoon/10250.js
@@ -0,0 +1,24 @@
+// 백준 10250번 ACM 호텔
+const filePath = process.platform === 'linux' ? 'dev/stdin' : './예제.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+const T = Number(input.shift());
+
+for (let i = 0; i < T; i++) {
+  const HWN = input[i].split(' ');
+  let H = Number(HWN.shift());
+  HWN.shift();
+  let N = Number(HWN.shift());
+  let count = 1;
+
+  while (N > H) {
+    count++;
+    N = N - H;
+  }
+  
+  if (count < 10) {
+    console.log(`${N}0${count}`);
+  }
+  else {
+    console.log(`${N}${count}`);
+  }
+}
