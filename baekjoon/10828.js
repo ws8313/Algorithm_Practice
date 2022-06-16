6baebd507abfba255a02b59db634efcc6624ae2d
ws8313 <ws8313@naver.com>
2022-06-17 00:36:22 +0900

    feat: solve boj 10828

--- baekjoon/10828.js
+++ baekjoon/10828.js
@@ -1,6 +1,6 @@
 // 백준 10828번 스택
 const filePath = process.platform === 'linux' ? 'dev/stdin' : './예제.txt';
-const input = require('fs').readFileSync(filePath).toString().split('\n');
+const input = require('fs').readFileSync(filePath).toString().split('\n').map(v => v.trim());
 const N = input.shift();
 const stack = [];
 // 경우마다 console.log를 찍어서 출력하면, 시간 초과
@@ -8,21 +8,21 @@ let answer = '';
 
 for (let i = 0; i < N; i++) {
   if (input[i].split(' ').length === 1) {
-    if (input[i].trim() === 'pop') {
+    if (input[i] === 'pop') {
       if (stack.length !== 0) {
         answer = answer + stack.pop() + '\n';
       } else {
         answer = answer + '-1\n';
       }
-    } else if (input[i].trim() === 'size') {
+    } else if (input[i] === 'size') {
       answer = answer + String(stack.length) + '\n';
-    } else if (input[i].trim() === 'empty') {
+    } else if (input[i] === 'empty') {
       if (stack.length === 0) {
         answer = answer + '1\n';
       } else {
         answer = answer + '0\n';
       }
-    } else if (input[i].trim() === 'top') {
+    } else if (input[i] === 'top') {
       if (stack.length !== 0) {
         answer = answer + stack[stack.length - 1] + '\n';
       } else {
