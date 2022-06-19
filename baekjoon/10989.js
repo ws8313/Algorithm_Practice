60edb77695b69b50504de1ceaebee8e741761fc3
ws8313 <ws8313@naver.com>
2022-06-20 06:29:30 +0900

    boj 10989

--- baekjoon/10989.js
+++ baekjoon/10989.js
@@ -1,5 +1,5 @@
 // 백준 10989번 수 정렬하기 3
-// node.js 메모리 부족으로 풀 수 없는 문젲
+// node.js 메모리 부족으로 풀 수 없는 문제
 const filePath = process.platform === 'linux' ? 'dev/stdin' : './예제.txt';
 const input = require('fs').readFileSync(filePath).toString().split('\n').map(v => v.trim());
 const N = input.shift();
